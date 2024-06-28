b2b303764a28913dc76deb0f38a6b25144e9d035
vasav-jain <vasavjain@gmail.com>
2024-06-28 15:23:16 -0400

    initial set up

---
+++ messageApp/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true,
+    },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:@typescript-eslint/recommended',
+      'prettier',
+    ],
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true,
+      },
+      ecmaVersion: 12,
+      sourceType: 'module',
+    },
+    plugins: [
+      'react',
+      '@typescript-eslint',
+    ],
+    rules: {
+      // Your custom rules
+    },
+  };
